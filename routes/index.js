f8cde16d68378a263dfc4bd42824b41daaf7c6cc
SuaveRav <41927993+SuaveRav@users.noreply.github.com>
2018-08-06 14:20:20 -0400

    mongo changes

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,11 @@ router.get('/helloworld', function(req, res, next){
   res.render('helloworld', {title : 'WHATUP' });
 })
 
+router.get('/userlist', function(req, res){
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {"userlist" : docs});
+  });
+});
 module.exports = router;
